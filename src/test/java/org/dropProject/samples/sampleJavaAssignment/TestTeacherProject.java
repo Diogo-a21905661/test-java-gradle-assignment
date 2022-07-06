5872202e85ef69f8852aa66c62ff0a21aeb1945a
Diogo Araújo <61624282+Diogo-a21905661@users.noreply.github.com>
2022-07-06 22:12:09 +0100

    Added base files
    Added base files to Sample Java Assignment.

---
+++ src/test/java/org/dropProject/samples/sampleJavaAssignment/TestTeacherProject.java
@@ -0,0 +1,30 @@
+package org.dropProject.samples.sampleJavaAssignment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+// in Drop Project, all test classes must begin with "Test"
+public class TestTeacherProject {
+    @Test
+    public void testFindMax() {
+        assertEquals(7, Main.findMax(new int[] { 1, 2, 7, 4 }));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindMaxWithNull() {
+        Main.findMax(null);
+    }
+
+    @Test
+    public void testFindMaxAllNegative() {
+        assertEquals(-1, Main.findMax(new int[]{-7, -5, -3, -1}));
+        assertEquals(-3, Main.findMax(new int[]{-7, -5, -3, -99}));
+    }
+
+    @Test
+    public void testFindMaxNegativeAndPositive() {
+        assertEquals(3, Main.findMax(new int[]{-7, -5, 3, -1}));
+        assertEquals(5, Main.findMax(new int[]{-7, 5, -3, -99}));
+    }
+}
